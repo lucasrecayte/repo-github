2dee5842635dd6edde99738e367e85e1c825f7b3
lucasrecayte <69016852+lucasrecayte@users.noreply.github.com>
2020-10-30 12:47:39 -0300

    grupal entrega 7

--- js/products.js
+++ js/products.js
@@ -1,35 +1,26 @@
 
 var ProductsArray = [];
+var ProductsArrayInicial = []; //para tener siempre cargado el array inicial
 
-function showProductsList(array) {
-
+function listaProductosCuadricula(array) {
     let htmlContentToAppend = "";
     for (let i = 0; i < array.length; i++) {
         let product = array[i];
 
-        htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action"  onclick="irAProductsInfo()">
-            <div class="row" > 
-
-                <div class="col-3">
-                    <img src="` + product.imgSrc + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ product.name + `</h4>
-                        <small class="text-muted">` + product.soldCount + ` vendidos</small>
-                    </div>
-                     <div>` + product.description + `</div>
-                     <br> 
-                     <div>` + `Costo:` + `  ` + product.cost + `  ` + product.currency + `</div>
-                                     
-                </div>
-            </div>
-        </div>
-        `
-
-
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+        htmlContentToAppend += `<div class="col-md-4 n" >
+        <a href="product-info.html" class="card mb-4 shadow-sm custom-card list-group-item-action" style="width: 300px; height:470px;">
+          <img class="bd-placeholder-img card-img-top" src= `+ product.imgSrc + `>
+          <h3 class="m-3">`+ product.name + `</h3>
+          <div class="card-body ">
+            <p class="card-text">` + product.description + `</p>
+            <p>` + `Costo:` + `  ` + product.cost + `  ` + product.currency + `</p>
+            <small class="text-muted">` + product.soldCount + ` vendidos</small>
+          </div>
+         
+        </a>
+      </div>
+    `
+        document.getElementById("cuadriculaProductos").innerHTML = htmlContentToAppend;
     }
 
 }
@@ -41,7 +32,7 @@ function OrdenarAsc_O_Desc(data, key, orden) {
         if (orden === 'asc') {                           //y esta debe ser una función que reciba como                       
             return ((x < y) ? -1 : ((x > y) ? 1 : 0));   //parámetros dos elementos del tipo con el que trabaja el array y 
         }                                                //devuelva un número negativo si el primer elemento debe ordenarse antes que el segundo, 
-                                                         //cero si ambos elementos tienen igual orden, o un número positivo si el segundo elemento 
+        //cero si ambos elementos tienen igual orden, o un número positivo si el segundo elemento 
         if (orden === 'desc') {                          // debe ordenarse antes que el primero.
             return ((x > y) ? -1 : ((x < y) ? 1 : 0));   // Operador Ternario --> Tiene la forma de: condition ? value-if-true : value-if-false
         }
@@ -49,45 +40,54 @@ function OrdenarAsc_O_Desc(data, key, orden) {
     });
 }
 function ordenarPrecioAsc() {
+    
     OrdenarAsc_O_Desc(ProductsArray, 'cost', 'asc');
-    showProductsList(ProductsArray)
+    listaProductosCuadricula(ProductsArray);
 
 }
 function ordenarPrecioDesc() {
+    
     OrdenarAsc_O_Desc(ProductsArray, 'cost', 'desc');
-    showProductsList(ProductsArray)
+    listaProductosCuadricula(ProductsArray);
 
 }
 function ordenarPorRelevanciaDesc() {
+    
     OrdenarAsc_O_Desc(ProductsArray, 'soldCount', 'desc')
-    showProductsList(ProductsArray)
+    listaProductosCuadricula(ProductsArray);
 }
 
 function filtrarPorPrecio() {
-    var min = document.getElementById("min").value,
-        max = document.getElementById("max").value;
-    var newArray = ProductsArray.filter(function (el) {
-        return el.cost <= max &&
-            el.cost >= min
-    });
-    if (newArray.length != 0){
-    showProductsList(newArray)
-    } else
-     document.getElementById("cat-list-container").innerHTML = ""
+    if (document.getElementById("min").value == "" || document.getElementById("max").value == "") {
+        document.getElementById("alertaproductos").innerHTML = `<div class="alert alert-danger" align="center">
+      <strong> ¡Por favor, ingrese precios máximo y mínimo!
+  </strong> 
+</div>`
+
+    } else {
+        document.getElementById("alertaproductos").innerHTML = "";
+        let min = document.getElementById("min").value,
+            max = document.getElementById("max").value;
+        let newArray = ProductsArray.filter(function (el) {
+            return el.cost <= max &&
+                el.cost >= min
+        });
+        if (newArray.length != 0) {
+            ProductsArray = newArray;    //para poder ordenar por precio lo filtrado
+            listaProductosCuadricula(ProductsArray);
+        } else
+            document.getElementById("cuadriculaProductos").innerHTML = ""
+    }
 }
-function limpiar(){
+function limpiar() {
     document.getElementById("min").value = ""
     document.getElementById("max").value = ""
-
-    showProductsList(ProductsArray)
+    ProductsArray = ProductsArrayInicial;    //vuelve todo a como estaba al principio
+    listaProductosCuadricula(ProductsArray);
 }
-function irAProductsInfo(){
+function irAProductsInfo() {
     window.location = "product-info.html";
 }
-
-
-
-
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -96,10 +96,9 @@ function irAProductsInfo(){
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
-            ProductsArray = resultObj.data;
-
-
-            showProductsList(ProductsArray);
+            ProductsArrayInicial = resultObj.data;
+            ProductsArray = ProductsArrayInicial;
+            listaProductosCuadricula(ProductsArrayInicial);
         }
     });
 });
